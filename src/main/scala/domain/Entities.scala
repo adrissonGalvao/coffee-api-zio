86b264a16cb88da368d82f0efc3f058e8cc78a6a
Adrisson <adrisson.galvao@gmail.com>
2020-01-04 22:51:04 -0300

    feat: list all product repository

--- src/main/scala/domain/Entities.scala
+++ src/main/scala/domain/Entities.scala
@@ -1,4 +1,4 @@
 package domain
 
-case class User(id: Long, fullName: String, email: String, password: String)
+case class User(id: Long, fullName: String, email: String)
 case class Product(id: Long, name: String, user: User)
--- src/main/scala/environment/effect/repository/ProductRepository.scala
+++ src/main/scala/environment/effect/repository/ProductRepository.scala
@@ -1,9 +1,10 @@
 package environment.effect.repository
 
-import domain.ProductCommand
+import domain.{Product, ProductCommand, User}
 import doobie.util.transactor.Transactor
 import environment.Environments.ProductEnvironment
-import failures.environmental.{RepositoryFailure}
+import failures.environmental.RepositoryFailure
+import failures.environmental.RepositoryFailure.RepositoryDefect
 import zio.{Task, ZIO}
 
 trait ProductRepository {
@@ -13,6 +14,7 @@ trait ProductRepository {
 object ProductRepository {
   trait Effect {
     def createProduct(productC: ProductCommand): ZIO[ProductEnvironment, RepositoryFailure, Unit]
+    def listAllProduct(): ZIO[ProductEnvironment, RepositoryFailure, List[Product]]
   }
 
   trait Live {
@@ -35,6 +37,38 @@ object ProductRepository {
             .transact(xa)
             .map(_ => ())
             .mapError(_ => RepositoryFailure.RepositoryDefect("not create product "))
+        }
+
+      override def listAllProduct(): ZIO[ProductEnvironment, RepositoryFailure, List[Product]] =
+        ZIO.accessM { _ =>
+          def statement =
+            sql"""
+                 |SELECT 
+                 |p.id,
+                 |p.name,
+                 |u.id,
+                 |u.full_name,
+                 |u.email,
+                 |FROM product p
+                 |INNER JOIN user u
+                 |ON u.id=p.user_id
+                 |""".stripMargin
+
+          val q = statement.query[(Long, String, Long, String, String)]
+
+          q.to[List]
+            .transact(xa)
+            .flatMap { els =>
+              els.map(t => {
+                val user = User(t._3, t._4, t._5)
+                Product(t._1, t._2, user)
+              }) match {
+                case lp: List[Product] => ZIO.succeed(lp)
+                case _                 => ZIO.fail(new Exception("error parser Data"))
+              }
+            }
+            .mapError(e => RepositoryDefect(e.getMessage))
+
         }
     }
 
